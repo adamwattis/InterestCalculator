b76a72a666bfef4acc8ea2ab48cd38d2fd36d51c
adamwattis <adamjwattis@gmail.com>
2020-03-19 21:38:12 -0400

    Spacing

--- src/components/frequency-selector.js
+++ src/components/frequency-selector.js
@@ -45,7 +45,7 @@ class FrequencySelector extends LitElement {
 					color: #00AEFF;
 					text-indent: 45%;
 					box-sizing: border-box;
-					margin-top: 7px;
+					margin-top: 10px;
 					background: #EBECF0;
 					border: none;
 					box-shadow: -10px -10px 20px 0 #FAFBFF, 10px 10px 20px 0 #A6ABBD;
